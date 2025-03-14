f1b5d69b1d10af9e8f58aeeac0f33fcbdc7f6148
DHARM <opdsbanasya@gmail.com>
2025-03-14 21:38:20 +0530

    folder module

---
+++ Episode 4/calculation/index.js
@@ -0,0 +1,4 @@
+const calculateSum2  = require("./sum2.js");
+const multiply = require("./multiply.js");
+
+module.exports = { calculateSum2, multiply };
---
+++ Episode 4/calculation/sum2.js
@@ -0,0 +1,5 @@
+function calculateSum2(a, b) {
+  return a + b;
+}
+
+module.exports = calculateSum2;
