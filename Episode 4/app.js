4300ccc0c2cc5ca59e9a9e05cd602bdfaf9d13fb
DHARM <opdsbanasya@gmail.com>
2025-03-14 21:48:40 +0530

    adding other things

--- Episode 4/app.js
+++ Episode 4/app.js
@@ -1,6 +1,26 @@
-// const { name, calculateSum} = require("./sum")
-import { name, calculateSum } from "./sum.js";
+const { name, calculateSum} = require("./sum")
+const { calculateSum2, multiply } = require("./calculation")
+const package = require("./package.json")
+const util = require("node:util")
+
+// import { name, calculateSum } from "./sum.js";
 
 var ans = calculateSum(20,50);
-console.log("Sum is: ", ans);
-console.log(name);
+
+// console.log("Sum is: ", ans);
+// console.log(name);
+
+var sum2 = calculateSum(21, 125);
+// console.log("Sum 2 is: ", sum2);
+
+var multi = multiply(50, 20);
+// console.log("Multi is: ",  multi);
+
+// console.log(package);
+
+// console.log(util);
+var txt = 'Congratulate %s on his %dth birthday!';
+var result = util.format(txt, 'Linus', 6);
+
+console.log(result);
+
--- Episode 4/sum.js
+++ Episode 4/sum.js
@@ -9,11 +9,15 @@ var name = "Lenovo";
 // When you have a single function or variable
 // module.exports = calculateSum;
 
+// 
+// console.log("module: ", module);
+// console.log("module.exports: ",module.exports);
+
 //* When you have multiple functions and variables
-// module.exports = {
-//     name: name,
-//     calculateSum: calculateSum
-// };
+module.exports = {
+    name: name,
+    calculateSum: calculateSum
+};
 
 // also you can export like
 // export { name, calculateSum };
