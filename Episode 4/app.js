388584cfa8486be1243ba63b841681411fb2427b
DHARM <opdsbanasya@gmail.com>
2025-03-14 20:58:50 +0530

    exporting modules by ejs

--- Episode 4/app.js
+++ Episode 4/app.js
@@ -1,4 +1,5 @@
-const { name, calculateSum} = require("./sum")
+// const { name, calculateSum} = require("./sum")
+import { name, calculateSum } from "./sum.js";
 
 var ans = calculateSum(20,50);
 console.log("Sum is: ", ans);
