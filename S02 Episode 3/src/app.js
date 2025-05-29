663bedc901c8f97d21fc093ddd61ce870c1444f8
DHARM <opdsbanasya@gmail.com>
2025-05-29 20:10:51 +0530

    creating a server using express

---
+++ S02 Episode 3/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Namaste MERN");
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/namaste", (req, res) => {
+  res.send("Namaste World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
